489d9c1108c24aa01b514aff39f0b48a09dae99b
Zachary Pahl <pahlzachary@gmail.com>
2021-03-01 18:45:47 -0700

    finished and debugged generatePassword function; added shuffle function

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,30 @@ var generateBtn = document.querySelector("#generate");
 
 // global variables
 var lengthCriteria = 8;
+var lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+var upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numerals = '1234567890';
+var specialAlphabet = '`~!@#$%^&*<>?=+-_{}[]:;';
+
+//selectrandom character function
+function randomChar(alphabet) {
+  return alphabet[Math.floor(Math.random()*alphabet.length)];
+}
+
+//shuffle function (Fisher-Yates shuffle)
+function shuffle(array) {
+  var m = array.length, t, i;
+  while (m) {
+    //Picks a remaining element from array
+    i = Math.floor(Math.random() *m--);
+
+    //swaps w/ current element
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
 
 // pwLength function
 function pwLength() {
@@ -21,37 +45,44 @@ function pwLength() {
 // generatePassword function
 function generatePassword() {
   var pwCharacters = [];
-  var caps, numerals, specials;
-  
   pwLength();
 
   // Caps
   if (confirm("Does password require capitalized characters?")) {
-    for (i = 0; i < Math.floor(Math.random() * (lengthCriteria - 3) + 1); i++) {
+    var counter = Math.floor(Math.random() * (lengthCriteria - 3) + 1);
+    for (i = 0; i < counter; i++) {
       //push random capitals to pwCharacters[]
-
-      lengthCriteria = lengthCriteria - i;
-      //lengthCriteria is now the "remaining" pw length
+      pwCharacters.push(randomChar(upperAlphabet));
+      //lengthCriteria to be the "remaining" pw length
     }
+    lengthCriteria = lengthCriteria - counter;
   }
 
   // Numerals
   if (confirm("Does password require numerals?")) {
-    for (i = 0; i < Math.floor(Math.random() * (lengthCriteria - 2) + 1); i++) {
-      //push random numbers to pwCharacters[]
-
-      lengthCriteria = lengthCriteria - i;
+    var counter = Math.floor(Math.random() * (lengthCriteria - 2) + 1);
+    for (i = 0; i < counter; i++) {
+      pwCharacters.push(randomChar(numerals));
     }
+    lengthCriteria = lengthCriteria - counter;
   }
 
   // special characters
   if (confirm("Does password require special characters?")) {
-    for (i = 0; i < Math.floor(Math.random() * (lengthCriteria - 1) + 1); i++) {
-      //push random special characters to pwCharacters[]
+    var counter = Math.floor(Math.random() * (lengthCriteria - 1) + 1);
+    for (i = 0; i < counter; i++) {
+      pwCharacters.push(randomChar(specialAlphabet));
     }
+    lengthCriteria = lengthCriteria - counter;
   }
 
-  //insert shuffle function here:
+  //push random lowercase letters for remaining length:
+  for (i=0; i<lengthCriteria; i++) {
+    pwCharacters.push(randomChar(lowerAlphabet));
+  }
+
+  //call shuffle function here (used Fisher-Yates shuffle):
+  shuffle(pwCharacters);
 
   //returns array as string w/ no spaces/commas b/t
   return pwCharacters.join("");
